2f710a93bb6549f141a11fb5695c012d64bebf27
Tony Parisi <tparisi@gmail.com>
2013-08-07 15:12:50 -0700

    Refining example

--- engine/src/system/system.js
+++ engine/src/system/system.js
@@ -1,6 +1,10 @@
 goog.provide("Vizi.System");
 
 Vizi.System = {
+	log : function() {
+		var args = [].slice.call(arguments);
+		console.log.apply(console, args);
+	},
 	warn : function() {
 		var args = ["Vizi Warning: "].concat([].slice.call(arguments));
 		console.warn.apply(console, args);
