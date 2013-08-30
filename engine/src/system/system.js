6d500c233561eca31ab17b06a2c4d8f444132096
Tony Parisi <tparisi@gmail.com>
2013-08-30 10:31:18 -0700

    Console cosmetics

--- engine/src/system/system.js
+++ engine/src/system/system.js
@@ -2,11 +2,15 @@ goog.provide("Vizi.System");
 
 Vizi.System = {
 	log : function() {
-		var args = [].slice.call(arguments);
+		var args = ["[Vizi] "].concat([].slice.call(arguments));
 		console.log.apply(console, args);
 	},
 	warn : function() {
-		var args = ["Vizi Warning: "].concat([].slice.call(arguments));
+		var args = ["[Vizi] "].concat([].slice.call(arguments));
 		console.warn.apply(console, args);
+	},
+	error : function() {
+		var args = ["[Vizi] "].concat([].slice.call(arguments));
+		console.error.apply(console, args);
 	}
 };
