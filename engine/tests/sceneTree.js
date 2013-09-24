b2ab263cc6a37b105ba762c3b6404d2f02c4c1c9
Tony Parisi <tparisi@gmail.com>
2013-09-23 17:26:18 -0700

    Property Pages

--- engine/tests/sceneTree.js
+++ engine/tests/sceneTree.js
@@ -68,34 +68,86 @@ selectSceneNode = function(viewer, node) {
 }
 
 vec3toString = function(vec) {
-	return "[" + vec.x.toFixed(6) + "," + vec.y.toFixed(6) + "," + vec.z.toFixed(6) + "]";
+	return "" + vec.x.toFixed(6) + "," + vec.y.toFixed(6) + "," + vec.z.toFixed(6) + "";
+}
+
+colorToString = function(color) {
+	return "" + color.r.toFixed(6) + "," + color.g.toFixed(6) + "," + color.b.toFixed(6) + "";
 }
 
 transformToString = function(transform) {
 
-	var str = "<b>Transform</b><br>pos: " + vec3toString(transform.position) +
-		"<br>rot:" + vec3toString(transform.position) +
-		"<br>scl:" + vec3toString(transform.scale);		
+	var str = "<b>Transform</b><br><table border='0' width='95%'><tr><td>Position:</td><td> " + vec3toString(transform.position) +
+		"</td><tr><td>Rotation:</td><td> " + vec3toString(transform.position) +
+		"</td><tr><td>Scale:</td><td> " + vec3toString(transform.scale) +
+		"</td></tr></tr></table>";		
 	
 	return str;
 }
 
-meshToString = function(mesh) {
-	return "(tbd)";
+meshToString = function(geometry) {
+	var str = "<b>Mesh</b><br>";
 
+	var nFaces = geometry.faces ? geometry.faces.length : geometry.attributes.index.array.length / 3;
+	var nVertices = geometry.vertices ? geometry.vertices.length :
+		geometry.attributes.position.array.length;
+	
+	str += (nFaces.toString() + " faces<br>");
+	str += (nVertices.toString() + " vertices<br>");
+	
+	if (!geometry.boundingBox)
+		geometry.computeBoundingBox();
+	
+	var size = geometry.boundingBox.size();
+	var boundingBoxSize = 
+		"[" + size.x.toFixed(2) + ", " + 
+	size.y.toFixed(2) + ", " + 
+	size.z.toFixed(2) +
+	"]<br>";
+
+	str += ("Bounds " + boundingBoxSize);
+	return str;
 }
 
 materialToString = function(material) {
-	return "(tbd)";
-}
-
-visualToString = function(visual) {
-
-	if (!visual)
-		return "<b>Visual</b>(null)";
+	if (material instanceof THREE.MeshFaceMaterial) {
+		material = material.materials[0];
+	}
 	
-	var str = "<b>Visual</b><br>mesh: " + meshToString(visual.mesh) +
-		"<br>material:" + materialToString(visual.material);
+	
+	var str = "<b>Material</b><br><table border='0' width='95%'>";
+	
+	var shading = "Unknown";
+	if (material instanceof THREE.MeshLambertMaterial) {
+		shading = "Lambert";
+	}
+	else if (material instanceof THREE.MeshPhongMaterial) {
+		shading = "Phong";
+	}
+	else if (material instanceof THREE.MeshBasicMaterial) {
+		shading = "Basic (unlit)";
+	}
+		
+	str += ("Shading: " + shading + "<br>");
+	
+	if (material.ambient) { 
+		str += ("<tr><td>Ambient:</td><td> " + colorToString(material.ambient));
+	}
+	
+	if (material.color) {
+		str += ("<tr><td>Diffuse:</td><td> " + colorToString(material.color));
+	}
+	
+	if (material.specular) {
+		str += ("</td><tr><td>Specular:</td><td> " + colorToString(material.specular));
+	}
+	
+	if (material.emissive) {
+		str += ("</td><tr><td>Emissive:</td><td> " + colorToString(material.emissive));
+	}
+	
+	str += ("</td><tr><td>Opacity:</td><td> " + material.opacity);
+	str += "</td></tr></tr></table>";		
 
 	return str;
 }
@@ -114,6 +166,12 @@ cameraToString = function(camera) {
 	return str;
 }
 
+groupToString = function(children) {
+
+	var str = "<b>Group</b>";		
+	
+	return str;
+}
 
 sceneNodeInfo = function(viewer, node) {
 
@@ -122,9 +180,11 @@ sceneNodeInfo = function(viewer, node) {
 	if (node.data.vizi) {
 		var object = node.data.vizi;
 		var transform = transformToString(object.transform);
-		var visual = visualToString(object.visuals && object.visuals.length ? object.visuals[0] : null);
+		var mesh = object.visuals && object.visuals.length ? meshToString(object.visuals[0].geometry) : null;
+		var material = object.visuals && object.visuals.length ? materialToString(object.visuals[0].material) : null;
 		var light = lightToString(object.light);
 		var camera = cameraToString(object.camera);
+		var group = groupToString(object.children);
 		info.object = {
 				name : object.name,
 				id : object._id,
@@ -141,9 +201,11 @@ sceneNodeInfo = function(viewer, node) {
 				id : object._id,
 				components : {
 					transform : transform,
-					visual : visual,
+					mesh : mesh,
+					material : material,
 					light : light,
-					camera : camera
+					camera : camera,
+					group : group
 				}
 		};
 		
