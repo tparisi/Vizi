51430b4d28e725afb1e7c0a778f3c1eb04b0374c
Tony Parisi <tparisi@gmail.com>
2014-03-04 14:40:22 -0800

    Added Vector and Plane helpers

--- engine/build/vizi-nodeps.js
+++ engine/build/vizi-nodeps.js
@@ -6126,10 +6126,10 @@ Vizi.PerspectiveCamera.prototype.update = function()  {
  */
 goog.provide('Vizi.Helpers');
 
-Vizi.Helpers.BoundingBoxDecoration = function(param)
-{
+Vizi.Helpers.BoundingBoxDecoration = function(param) {
 	param = param || {};
 	if (!param.object) {
+		Vizi.warn("Vizi.Helpers.BoundingBoxDecoration requires an object");
 		return null;
 	}
 	
@@ -6153,6 +6153,75 @@ Vizi.Helpers.BoundingBoxDecoration = function(param)
 	
 	return decoration;
 }
+
+Vizi.Helpers.VectorDecoration = function(param) {
+
+	param = param || {};
+	
+	var start = param.start || new THREE.Vector3;
+	var end = param.end || new THREE.Vector3(0, 1, 0);
+	var color = param.color !== undefined ? param.color : 0x888888;
+	
+	var linegeom = new THREE.Geometry();
+	linegeom.vertices.push(start, end); 
+
+	var mat = new THREE.LineBasicMaterial({color:color});
+
+	var mesh = new THREE.Line(linegeom, mat);
+	
+	var decoration = new Vizi.Decoration({object:mesh});
+	return decoration;
+}
+
+Vizi.Helpers.PlaneDecoration = function(param) {
+
+	param = param || {};
+	
+	if (!param.normal && !param.triangle) {
+		Vizi.warn("Vizi.Helpers.PlaneDecoration requires either a normal or three coplanar points");
+		return null;
+	}
+
+	var normal = param.normal;
+	if (!normal) {
+		// do this later
+		Vizi.warn("Vizi.Helpers.PlaneDecoration creating plane from coplanar points not implemented yet");
+		return null;
+	}
+	
+	var position = param.position || new THREE.Vector3;	
+	var size = param.size || 1;
+	var color = param.color !== undefined ? param.color : 0x888888;
+	
+	var u = new THREE.Vector3(0, normal.z, -normal.y).normalize().multiplyScalar(size);
+	var v = u.clone().cross(normal).normalize().multiplyScalar(size);
+	
+	var p1 = position.clone().sub(u).sub(v);
+	var p2 = position.clone().add(u).sub(v);
+	var p3 = position.clone().add(u).add(v);
+	var p4 = position.clone().sub(u).add(v);
+	
+	var planegeom = new THREE.Geometry();
+	planegeom.vertices.push(p1, p2, p3, p4); 
+	var planeface = new THREE.Face3( 0, 1, 2 );
+	planeface.normal.copy( normal );
+	planeface.vertexNormals.push( normal.clone(), normal.clone(), normal.clone(), normal.clone() );
+	planegeom.faces.push(planeface);
+	var planeface = new THREE.Face3( 0, 2, 3 );
+	planeface.normal.copy( normal );
+	planeface.vertexNormals.push( normal.clone(), normal.clone(), normal.clone(), normal.clone() );
+	planegeom.faces.push(planeface);
+	planegeom.computeFaceNormals();
+	planegeom.computeCentroids();
+
+	var mat = new THREE.MeshBasicMaterial({color:color, transparent: true, side:THREE.DoubleSide, opacity:0.1 });
+
+	var mesh = new THREE.Mesh(planegeom, mat);
+	
+	var decoration = new Vizi.Decoration({object:mesh});
+	return decoration;
+}
+
 /**
  * @fileoverview Vizi scene utilities
  * @author Tony Parisi
--- engine/tests/editor.js
+++ engine/tests/editor.js
@@ -64,48 +64,22 @@ Vizi.Editor.prototype.showNormal = function(object, event) {
 			var hitpoint = event.point.clone();
 			var hitnormal = event.normal.clone();
 			var vec = hitpoint.clone().add(hitnormal.multiplyScalar(normalLength));
-			var linegeom = new THREE.Geometry();
-			linegeom.vertices.push(hitpoint, vec); 
-
-			var mat = new THREE.LineBasicMaterial({color:0xaa0000 });
-
-			var mesh = new THREE.Line(linegeom, mat);
 			
-			this.normalDecoration = new Vizi.Decoration({object:mesh});
+			this.normalDecoration = Vizi.Helpers.VectorDecoration({
+				start : hitpoint,
+				end : vec,
+				color : 0xaa0000
+			});
+						
 			this.highlightedObject._parent.addComponent(this.normalDecoration);
 					
 			// The plane
 			var planeSize = Math.max(width, Math.max(height, depth));
 
-			var u = new THREE.Vector3(0, event.normal.z, -event.normal.y).normalize().multiplyScalar(Vizi.Editor.DRAG_PLANE_SIZE);
-			var v = u.clone().cross(event.normal).multiplyScalar(4);
-			
-			var cx = event.point.x;
-			var cy = event.point.y;
-			
-			var p1 = event.point.clone().sub(u).sub(v);
-			var p2 = event.point.clone().add(u).sub(v);
-			var p3 = event.point.clone().add(u).add(v);
-			var p4 = event.point.clone().sub(u).add(v);
-			
-			var planegeom = new THREE.Geometry();
-			planegeom.vertices.push(p1, p2, p3, p4); 
-			var planeface = new THREE.Face3( 0, 1, 2 );
-			planeface.normal.copy( event.normal );
-			planeface.vertexNormals.push( event.normal.clone(), event.normal.clone(), event.normal.clone(), event.normal.clone() );
-			planegeom.faces.push(planeface);
-			var planeface = new THREE.Face3( 0, 2, 3 );
-			planeface.normal.copy( event.normal );
-			planeface.vertexNormals.push( event.normal.clone(), event.normal.clone(), event.normal.clone(), event.normal.clone() );
-			planegeom.faces.push(planeface);
-			planegeom.computeFaceNormals();
-			planegeom.computeCentroids();
-
-			var mat = new THREE.MeshBasicMaterial({color:0x888888, transparent: true, side:THREE.DoubleSide, opacity:0.1 });
-
-			var mesh = new THREE.Mesh(planegeom, mat);
-			
-			this.planeDecoration2 = new Vizi.Decoration({object:mesh});
+			this.planeDecoration2 = Vizi.Helpers.PlaneDecoration({
+				normal: event.normal,
+				position:event.point,
+				size:planeSize});
 			this.highlightedObject._parent.addComponent(this.planeDecoration2);
 		
 		}
